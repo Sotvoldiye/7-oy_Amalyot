1a65f429c0fba6242e65236effda031ab9d93552
Ulgbbek-js <dilafruztoshmatova209@gmail.com>
2025-04-28 10:05:23 +0500

    setting all

--- src/hooks/useLogin.js
+++ src/hooks/useLogin.js
@@ -8,31 +8,23 @@ import { login, logout } from "../app/features/userSlice";
 export const useLogin = () => {
   const dispatch = useDispatch();
 
-  const [data, setData] = useState(null);     
-  const [isPending, setIsPending] = useState(false); 
+  const [data, setData] = useState(null);
+  const [isPending, setIsPending] = useState(false);
 
   const signIn = async (email, password) => {
     setIsPending(true);
     try {
-     
       const res = await signInWithEmailAndPassword(auth, email, password);
       const user = res.user;
 
-      
       dispatch(login(user));
 
-     
       setData(user);
 
-     
       toast.success(`Xush kelibsiz, ${user.displayName}!`);
     } catch (err) {
-      
       toast.error(err.message);
       console.error(err);
-
-      
-     
     } finally {
       setIsPending(false);
     }
--- src/pages/budgets/Budgets.jsx
+++ src/pages/budgets/Budgets.jsx
@@ -1,8 +1,29 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import style from "./Budgets.module.scss"
+import { Chart } from '../../components/Chart/ApexChart'
+import { useCollectionsData } from '../../hooks/useCollectionsData';
 const Budgets = () => {
+    const { data, isPending } = useCollectionsData();
+    const [pots, setPots] = useState([]);
+    const [total, setTotal] = useState(0);
+    const [budgets, setBudgets] = useState([])
+    const [budgetTotal, setBudgetTotal] = useState(0)
+    useEffect(() => {
+        if (data && data.pots) {
+            setPots(data.pots);
+              const totalSum = data.pots.reduce((acc, item) => acc + item.total, 0);
+            setTotal(totalSum);
+        }
+        if(data && data.budgets){
+            setBudgets(data.budgets)
+            const totalBudgets = data.budgets.reduce((acc, item)=> acc + item.maximum, 0)
+            setBudgetTotal(totalBudgets)
+        }
+    }, [data]);
   return (
-    <div>Budgets</div>
+    <div>
+                  {data ? <Chart budgetTotal={budgetTotal} budgets={data.budgets} /> :   <p>Loading ...</p>           }
+    </div>
   )
 }
 
